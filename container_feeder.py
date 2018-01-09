80c3265b19a6ea5bcffd2fb8dd91567346c4d751
Me <me@N-115-RAKOCZI.local>
2018-01-09 23:21:09 +0000

    worker deletion in container_feeder

--- container_feeder.py
+++ container_feeder.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, unicode_literals
-import shlex, sys, time, docker, subprocess, ast, redis
+import shlex, sys, time, docker, subprocess, ast, redis, os, signal
 
 from pprint import pprint
 from threading import Thread
@@ -8,25 +8,29 @@ from docker import types
 import monitoring
 from parameters import backend_experiment_db
 
-job_workers = []
+job_workers = {}
 node_id = "no_id_3"
 customer_services = {}
+
 
 def worker(container):
 	# Arguments you give on command line
 	global node_id
 	monitoring.add_worker(node_id, container["service_name"])
-	output = subprocess.check_output(['python3','jqueuing_worker.py', str(node_id) ,str(container)])
-	monitoring.terminate_worker(node_id, service_name)
-	print("Worker_main Output: " + str(output))
+	process = subprocess.Popen(['python3','jqueuing_worker.py', str(node_id) ,str(container)])
+	print("process: " + str(process))
+	container['process'] = process
+	#job_workers[container['container_long_id']] = {'container': container, 'process': process}
+	#monitoring.terminate_worker(node_id, service_name)
+	#print("Worker_main Output: " + str(output))
 
 def add(container):
-	print("**************************************")
-	print(container)
+	#print("**************************************")
+	#print(container)
 	job_worker_thread = Thread(target = worker, args = (container,))
 	job_worker_thread.start()
-	job_workers.append(job_worker_thread)
-	print("**************Back from worker ************")
+	#job_workers.append(job_worker_thread)
+	#print("**************Back from worker ************")
 
 def start(node_id_t):
 	global node_id
@@ -41,7 +45,9 @@ def start(node_id_t):
 		raise e
 	print("Docker Client: " + str(client))
 	print("After client init " + node_id)
+	current_update = 0
 	while True:
+		current_update += 1
 		#print("New round - container feeder")
 		for container in client.containers.list():
 			#pprint(container.attrs)
@@ -57,27 +63,13 @@ def start(node_id_t):
 					#print("Container " + container_long_id + " belongs to non-watched service")
 					continue
 				experiment = ast.literal_eval(backend_experiment_db.get(container_service_name))
-				#print(experiment)
-				#print("Container " + container_long_id + " belongs to a watched service")
-				#print("container_list: " + str(container_list))
+
 				if (container_long_id in container_list):
 					#print("Container " + container_long_id + " has been added previously")
+					 container_list[container_long_id]['current_update'] = current_update
 					continue
 				print("Container " + container_long_id + " will be added now")
-				'''
-				print('id_long:' + container_long_id)
-				print('name' + container.attrs['Name'])
-				print('service_id' + container.attrs['Config']['Labels']['com.docker.swarm.service.id'])
-				print('service_name' + container_service_name)
-				print('task_id' + container.attrs['Config']['Labels']['com.docker.swarm.task.id'])
-				print('task_name' + container.attrs['Config']['Labels']['com.docker.swarm.task.name'])
-				print('hostname'  + container.attrs['Config']['Hostname'])
-				print('ip_address' + '')
-				print('created'+ container.attrs['Created'])
-				print('started' + container.attrs['State']['StartedAt'])
-				print('experiment_id' + experiment['experiment_id'])
-				print('experiment_params' + str(experiment['params'])) 	
-				'''	
+
 				container_obj = {
 					'id_long': container_long_id,
 					'name': container.attrs['Name'], 
@@ -90,25 +82,28 @@ def start(node_id_t):
 					'created': container.attrs['Created'],
 					'started': container.attrs['State']['StartedAt'],
 					'experiment_id':experiment['experiment_id'], 
+					'current_update': current_update ,
 				}
-				'''		
-					'experiment_params':experiment['params'], 
-				}
-				'''
-				#print("After the container_obj")
-				#print(str(container_obj))
+
 				try:
 					container_obj['ip_address'] = container.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-					#print("BEFORE print container_obj")
-					#pprint(container_obj)
+
 					add(container_obj)
+					pprint(container_obj)
 					container_list[container_long_id] = container_obj
-					#pprint(container_obj)
-					#print("AFTER print container_obj")
+
 				except Exception as e:
 					print("An error happened while sending the container to the Agent")
 					pass
 			except Exception as e:
 				#print("It isn't a swarm service's container")
 				pass
+			trash = []
+			for container_id_temp in container_list:
+				container_temp = container_list[container_id_temp]
+				if (container_temp['current_update'] < current_update):
+					os.kill(container_temp['process'].pid, signal.SIGINT)
+					trash.append(container_id_temp)
+			for x in trash:
+				del container_list[x]
 		time.sleep(5)
